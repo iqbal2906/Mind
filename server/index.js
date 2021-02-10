04523e0b36ee48a2815dc4f5e3fd402a547fab5c
iqbal2906 <maniacjoe3@gmail.com>
2021-02-11 05:02:43 +0700

    setting up to push to Heroku

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Gallery Q");
+});
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 3000;
 
